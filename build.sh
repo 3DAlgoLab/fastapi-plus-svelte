c7748b02ff6bd7ca4a888fba082c94245fbd8e22
3DAlgoLab <threealgo@outlook.com>
2023-10-30 02:08:13 +0900

    first commit

---
+++ build.sh
@@ -0,0 +1,4 @@
+cd client
+npm run build
+cd ..
+uvicorn main:app --reload
---
+++ main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+# Place After All Other Routes
+app.mount("", StaticFiles(directory="client/dist/", html=True), name="static")
